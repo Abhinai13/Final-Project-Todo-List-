1e14b27d87726a2f43f7d28d0b77e68e029a5ed9
ABHINAIDT\Abhinai <Abhinai13@gmail.com>
2017-12-18 00:02:42 -0500

    config change

--- Final/config/config.php
+++ Final/config/config.php
@@ -1,7 +1,7 @@
 <?php
 define("host", "sql2.njit.edu");
 define("dbname", "ap565");
-define("user", "ap565@webhost01.ucs.njit.edu");
+define("user", "ap565");
 define("pass", "t9IUwE7RF");
 define('__ROOT__', dirname(dirname(__FILE__))); 
 
